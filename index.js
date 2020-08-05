6b073b823743f01292948e8b898f17a91343ad39
khill <kalisha.hill@gmail.com>
2020-08-05 16:27:01 -0500

    drum website that will play drum sounds when specific keys are pressed

---
+++ index.js
@@ -0,0 +1,61 @@
+// Loop thru the document
+for (var i=0; i<document.querySelectorAll(".drum").length; i++) {
+
+    // grab any element with the class of drum, and loop through each of those elements.
+    // Add an event listener to each and "listen" for clicks. Click will trigger the function
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML)
+        
+    });
+}
+
+// Event listern that will make a sound when we press the keys associated with the buttons
+document.addEventListener('keydown', function (event) {
+    // console.log(`${event.key} was pressed`)
+    makeSound(event.key);
+    buttonAnimation(event.key)
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w": 
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        default:
+            console.log(buttonInnerHTML);
+    }
+}
+
+function buttonAnimation (currentKey) {
+    let activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("active");
+
+}
